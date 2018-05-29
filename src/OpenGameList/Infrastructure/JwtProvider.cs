a60a5e0094b1a961cb91aa26c985c0d767f179b0
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-05-29 07:57:32 -0500

    Inicia la implementación para usar JWT(Json Web Token)

---
+++ src/OpenGameList/Infrastructure/JwtProvider.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using OpenGameList.Data.Users;
+using Microsoft.AspNetCore.Identity;
+using OpenGameList.Data;
+using System.Text;
+
+namespace OpenGameList.Infrastructure
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class JwtProvider
+    {
+        #region private members
+        private readonly RequestDelegate _next;
+
+        // JWT-related members
+        private TimeSpan TokenExpiration;
+        private SigningCredentials SigningCredentials;
+
+        // EF and Identity members, available through DI
+        private ApplicationDbContext DbContext;
+        private UserManager<ApplicationUser> UserManager;
+        private SignInManager<ApplicationUser> SignInManager;
+        #endregion
+
+        #region Static members
+        private static readonly string PrivateKey = "private_key_1234567890";
+        public static readonly SymmetricSecurityKey SecurityKey
+            = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(PrivateKey));
+        public static readonly string Issuer = "OpenGameListWebApp";
+        public static string TokenEndpoint = "/api/connect/token";
+        #endregion
+
+        #region Constructor
+        public JwtProvider(
+            RequestDelegate next,
+            ApplicationDbContext dbContext,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signinManager)
+        {
+            _next = next;
+
+            // Instantiate JWT-related members
+            TokenExpiration = TimeSpan.FromMinutes(10);
+            SigningCredentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
+
+            // Instantiate through Dependency Injection
+            DbContext = dbContext;
+            UserManager = userManager;
+            SignInManager = signinManager;
+        }
+        #endregion
+
+        #region public methods
+        public Task Invoke(HttpContext httpContext)
+        {
+            // Check if the request path matches out TokenEndPoint
+            if (!httpContext.Request.Path.Equals(TokenEndpoint, StringComparison.Ordinal))
+            {
+                return _next(httpContext);
+            }
+
+            // Check if the current request is a valid POST with the appropriate content type (application/x-www-form-urlencoded)
+            if (httpContext.Request.Method.Equals("POST")
+                    && httpContext.Request.HasFormContentType)
+            {
+                //  Ok: generate token and send it via a json-formatted string 
+                return CreateToken(httpContext);
+            }
+            else
+            {
+                // Not Ok: output a 400 - Bad Request Http error.
+                httpContext.Response.StatusCode = 400;
+                return httpContext.Response.WriteAsync("Bad request.");
+            }
+        }
+        #endregion
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class JwtProviderExtensions
+    {
+        public static IApplicationBuilder UseJwtProvider(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<JwtProvider>();
+        }
+    }
+}
